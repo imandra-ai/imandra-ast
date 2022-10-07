22bbc2d3e78fac9c3d863d1ed5e5b569131adb09
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-10-07 11:19:28 -0400

    builders for decl

--- src/decl.ml
+++ src/decl.ml
@@ -9,3 +9,10 @@ and view =
   | Ty of Type.def list
   | Fun of Term.fun_decl list
 [@@deriving show, yojson]
+
+open struct
+  let mk_ ~loc view : t = { loc; view }
+end
+
+let ty ~loc defs : t = mk_ ~loc @@ Ty defs
+let fun_ ~loc defs : t = mk_ ~loc @@ Fun defs
