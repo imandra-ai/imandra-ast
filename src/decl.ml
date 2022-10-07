b7046f21935fc09e41b4239ad912ff3c3c51244a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-10-07 12:13:03 -0400

    modify decl for funs

--- src/decl.ml
+++ src/decl.ml
@@ -7,12 +7,15 @@ type t = {
 
 and view =
   | Ty of Type.def list
-  | Fun of Term.fun_decl list
-[@@deriving show, yojson]
+  | Fun of {
+      recursive: bool;
+      fs: Term.fun_decl list;
+    }
+[@@deriving show { with_path = false }, yojson]
 
 open struct
   let mk_ ~loc view : t = { loc; view }
 end
 
 let ty ~loc defs : t = mk_ ~loc @@ Ty defs
-let fun_ ~loc defs : t = mk_ ~loc @@ Fun defs
+let fun_ ~loc ~recursive defs : t = mk_ ~loc @@ Fun { recursive; fs = defs }
