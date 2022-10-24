3b423134ed24762f9ddda9c386cccb1b2c6f446a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-10-24 13:53:39 -0400

    feat(decl): (de)ser for lists of decls

--- src/decl.ml
+++ src/decl.ml
@@ -14,9 +14,19 @@ and view =
     }
 [@@deriving show { with_path = false }, yojson]
 
+type list_of = t list [@@deriving show, yojson]
+
 open struct
   let mk_ ~loc view : t = { loc; view }
 end
 
 let ty ~loc defs : t = mk_ ~loc @@ Ty defs
 let fun_ ~loc ~recursive defs : t = mk_ ~loc @@ Fun { recursive; fs = defs }
+
+let ty_defs_of_decls (decls : t list) : Type.Defs.t =
+  decls
+  |> CCList.flat_map (fun d ->
+         match d.view with
+         | Ty defs -> defs
+         | _ -> [])
+  |> List.fold_left (fun defs d -> Type.Defs.add d defs) Type.Defs.empty
