20641e1b7ad9f0bd573cdb5e5fbd797ac591471d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-11-07 15:59:46 -0500

    feat: improve codegen for OCaml (wip: codegen functions)

--- src/cg-ocaml/imandra_ast_cg_ocaml.ml
+++ src/cg-ocaml/imandra_ast_cg_ocaml.ml
@@ -21,6 +21,7 @@ type kind =
   | K_fun
   | K_ty
   | K_var
+  | K_mod
 
 let prelude = {ocaml|
 open Imandra_prelude;;
@@ -63,7 +64,7 @@ let str_of_id (self : state) (id : Uid.t) (kind : kind) : string =
             | _ -> mod_name @ [ base_name ]
           in
           String.concat "" l
-        | K_cstor -> String.capitalize_ascii base_name
+        | K_mod | K_cstor -> String.capitalize_ascii base_name
         | K_ty_var ->
           (* remove the "'" *)
           String.capitalize_ascii (String.sub name 1 (String.length name - 1))
@@ -121,6 +122,166 @@ let cg_ty ?(clique = []) (self : state) (out : Buffer.t) (ty : Type.t) : unit =
   let ty = Type.chase_deep self.ty_defs ty in
   recurse out ty
 
+let cg_const (out : Buffer.t) (c : Term.const) : unit =
+  match c with
+  | Term.Const_nativeint n -> bpf out "%ndn" n
+  | Term.Const_int32 n -> bpf out "%ldl" n
+  | Term.Const_int64 n -> bpf out "%LdL" n
+  | Term.Const_float f -> bpf out "%h" f
+  | Term.Const_char c -> bpf out "%C" c
+  | Term.Const_string s -> bpf out "%S" s
+  | Term.Const_z s -> bpf out "(Z.of_string %S)" (Z.to_string s)
+  | Term.Const_q q -> bpf out "(Q.of_string %S)" (Q.to_string q)
+  | Term.Const_real_approx _s ->
+    bpf out "assert false (* TODO: real approx %s *)" _s
+
+let cg_pat (self : state) (out : Buffer.t) (p : Term.pattern) : unit =
+  let rec recurse out (p : Term.pattern) : unit =
+    match p.view with
+    | Term.P_var v -> bpf out "%s" (str_of_id self v.id K_var)
+    | Term.P_tuple (_, l) ->
+      bpf out "(";
+      List.iteri
+        (fun i x ->
+          if i > 0 then bpf out ",";
+          recurse out x)
+        l;
+      bpf out ")"
+    | Term.P_any _ -> bpf out "_"
+    | Term.P_true -> bpf out "true"
+    | Term.P_false -> bpf out "false"
+    | Term.P_const (c, _) -> cg_const out c
+    | Term.P_or (a, b) -> bpf out "(%a | %a)" recurse a recurse b
+    | Term.P_construct { c; args = []; _ } ->
+      bpf out "%s" (str_of_id self c K_cstor)
+    | Term.P_construct { c; args; lbls = None; _ } ->
+      bpf out "%s(" (str_of_id self c K_cstor);
+      List.iteri
+        (fun i ty ->
+          if i > 0 then bpf out ",";
+          recurse out ty)
+        args;
+      bpf out ")"
+    | Term.P_construct { c; args; lbls = Some lbls; _ } ->
+      assert false (* TODO *)
+    | Term.P_record (_, rows) ->
+      bpf out "{";
+      List.iteri
+        (fun i (f, p) ->
+          if i > 0 then bpf out ";";
+          bpf out "%s: %a" (str_of_id self f K_field) recurse p)
+        rows;
+      bpf out "}"
+    | Term.P_alias (v, p) ->
+      bpf out "(%a as %s)" recurse p (str_of_id self v.id K_var)
+  in
+  recurse out p
+
+let str_of_apply_arg = function
+  | Term.Nolabel -> ""
+  | Term.Label s -> spf "~%s" s
+  | Term.Optional s -> spf "?%s" s
+
+let is_infix s =
+  s <> ""
+  &&
+  match s.[0] with
+  | 'a' .. 'z' | 'A' .. 'Z' | '_' -> false
+  | _ -> true
+
+let rec cg_term (self : state) (out : Buffer.t) (t : Term.t) : unit =
+  let rec recurse out (t : Term.t) : unit =
+    match Term.view t with
+    | Term.Const (c, _) -> cg_const out c
+    | Term.If (a, b, c) ->
+      bpf out "(if %a\n then %a\n else %a)" recurse a recurse b recurse c
+    | Term.Let (rec_, bs, bod) ->
+      bpf out "let%s "
+        (if rec_ = Term.Recursive then
+          " rec"
+        else
+          "");
+      List.iteri
+        (fun i ((x : Var.t), t) ->
+          if i > 0 then bpf out " and ";
+          bpf out "%s = %a\n" (str_of_id self x.id K_var) recurse t)
+        bs;
+      bpf out "in\n%a" recurse bod
+    | Term.Apply (_, f, []) -> recurse out f
+    | Term.Apply (_, { Term.view = Term.Ident f; _ }, [ a; b ])
+      when is_infix (Uid.name f.id) ->
+      bpf out "(%a %s %a)" recurse_arg a (Uid.name f.id) recurse_arg b
+    | Term.Apply (_, f, args) ->
+      bpf out "(%a" recurse f;
+      List.iter (fun a -> bpf out " %a" recurse_arg a) args;
+      bpf out ")"
+    | Term.Ident x -> bpf out "%s" (str_of_id self x.id K_var)
+    | Term.Tuple (_, l) ->
+      bpf out "(";
+      List.iteri
+        (fun i x ->
+          if i > 0 then bpf out ", ";
+          recurse out x)
+        l;
+      bpf out ")"
+    | Term.True -> bpf out "true"
+    | Term.False -> bpf out "false"
+    | Term.Fun (_, lbl, x, bod) ->
+      bpf out "(fun %s%s ->\n %a)" (str_of_apply_arg lbl)
+        (str_of_id self x.id K_var)
+        recurse bod
+    | Term.Field { f; t; _ } ->
+      bpf out "%a.%s" recurse t (str_of_id self f K_field)
+    | Term.Construct { c; args = []; _ } ->
+      bpf out "%s" (str_of_id self c K_cstor)
+    | Term.Construct { c; args; lbls = None; _ } ->
+      bpf out "%s(" (str_of_id self c K_cstor);
+      List.iteri
+        (fun i x ->
+          if i > 0 then bpf out ", ";
+          recurse out x)
+        args;
+      bpf out ")"
+    | Term.Construct { c; args; lbls = Some lbls; _ } -> assert false (* TODO *)
+    | Term.Record (_, rows, rest) ->
+      (match rest with
+      | None -> bpf out "{"
+      | Some r -> bpf out "{(%a) with " recurse r);
+      List.iteri
+        (fun i (f, x) ->
+          if i > 0 then bpf out "; ";
+          bpf out "%s=%a" (str_of_id self f K_field) recurse x)
+        rows;
+      bpf out "}"
+    | Term.As (t, ty) -> bpf out "(%a : %a)" recurse t (cg_ty self) ty
+    | Term.Match { lhs; bs; _ } ->
+      bpf out "(match %a with\n" recurse lhs;
+      List.iter (fun vb -> bpf out "| %a\n" (cg_vb self) vb) bs;
+      bpf out ")"
+    | Term.Let_match { flg; bs; body; _ } ->
+      bpf out "let%s "
+        (match flg with
+        | Term.Recursive -> " rec"
+        | _ -> "");
+      List.iteri
+        (fun i vb ->
+          if i > 0 then bpf out "\n and ";
+          bpf out "%a" (cg_vb self) vb)
+        bs;
+      bpf out "in %a" recurse body
+  and recurse_arg out (arg, a) =
+    let s = str_of_apply_arg arg in
+    bpf out " %s%a" s recurse a
+  in
+
+  recurse out t
+
+and cg_vb (self : state) out (vb : Term.t Term.vb) : unit =
+  let { Term.pat; when_; expr } = vb in
+  bpf out "%a" (cg_pat self) pat;
+  Option.iter (fun e -> bpf out " when %a" (cg_term self) e) when_;
+  bpf out " -> %a" (cg_term self) expr
+
 let cg_ty_decl (self : state) ~clique (out : Buffer.t) (ty_def : Type.def) :
     unit =
   let name = str_of_id self ty_def.name K_ty in
@@ -134,7 +295,6 @@ let cg_ty_decl (self : state) ~clique (out : Buffer.t) (ty_def : Type.def) :
 
   (match ty_def.decl with
   | Type.Record rows ->
-    (* bpf out "#[derive(Eq,PartialEq,Clone,Debug)]\n"; *)
     bpf out "type %s%s = {\n" name args;
     List.iter
       (fun { Type.f; ty } ->
@@ -143,7 +303,6 @@ let cg_ty_decl (self : state) ~clique (out : Buffer.t) (ty_def : Type.def) :
 
     bpf out "}"
   | Type.Algebraic cstors ->
-    (* bpf out "#[derive(Eq,PartialEq,Clone,Debug)]\n"; *)
     bpf out "type %s%s = \n" name args;
     List.iter
       (fun { Type.c; args; labels } ->
@@ -175,16 +334,37 @@ let cg_ty_decl (self : state) ~clique (out : Buffer.t) (ty_def : Type.def) :
   | Type.Other | Type.Skolem -> bpf out "(* (other) *)\ntype %s%s;" name args);
   bpf out "\n\n"
 
-let cg_fun (self : state) (out : Buffer.t) (f : Term.fun_decl) : unit =
-  () (* TODO *)
+let cg_fun (self : state) ~sep (out : Buffer.t) (f : Term.fun_decl) : unit =
+  bpf out "%s %a = %a\n" sep (cg_pat self) f.pat (cg_term self) f.body
 
-let cg_decl (self : state) (d : Decl.t) : unit =
+let rec cg_decl (self : state) (d : Decl.t) : unit =
   match d.view with
   | Decl.Ty { tys = defs } ->
     let clique = List.map (fun d -> d.Type.name) defs in
     List.iter (cg_ty_decl self ~clique self.out) defs
-  | Decl.Fun { recursive = _; fs } -> List.iter (cg_fun self self.out) fs
-  | Decl.Module_alias (name, _) | Decl.Module { name; _ } -> () (* TODO *)
+  | Decl.Fun { recursive; fs } ->
+    List.iteri
+      (fun i f ->
+        let sep =
+          if i = 0 then
+            if recursive then
+              "let rec"
+            else
+              "let"
+          else
+            "and"
+        in
+        cg_fun ~sep self self.out f)
+      fs;
+    bpf self.out "\n"
+  | Decl.Module_alias (name, m) ->
+    bpf self.out "module %s = %s"
+      (str_of_id self name K_mod)
+      (str_of_id self m K_mod)
+  | Decl.Module { name; items } ->
+    bpf self.out "module %s = struct\n" (str_of_id self name K_mod);
+    List.iter (cg_decl self) items;
+    bpf self.out "end"
 
 let codegen (decls : Decl.t list) : string =
   let ty_defs = Decl.ty_defs_of_decls decls in
