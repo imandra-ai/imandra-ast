e1228d6b886cb10630843f17d891f5e9c3e5eccc
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-11-18 12:41:28 -0500

    move prelude into its own file

--- src/cg-ocaml/imandra_ast_cg_ocaml.ml
+++ src/cg-ocaml/imandra_ast_cg_ocaml.ml
@@ -23,27 +23,7 @@ type kind =
   | K_var
   | K_mod
 
-let prelude =
-  {ocaml|
-open Imandra_prelude;;
-
-module DJ_Z = struct
-  type t = Z.t
-  let to_yojson t = `Int (Z.to_int t)
-  let of_yojson = function
-    | `Int i -> Ok (Z.of_int i)
-    | _ -> Error "DJ_Z.of_yojson"
-end
-
-module DJ_Q = struct
-  type t = Q.t
-  let to_yojson t = `Float (Q.to_float t)
-  let of_yojson = function
-    | `Float f -> Ok (Q.of_float f)
-    | _ -> Error "DJ_Q.of_yojson"
-end
-
-|ocaml}
+let prelude = Prelude_code.code
 
 (* generate unique name from [base] and counter *)
 let gensym (self : state) (base : string) : string =
